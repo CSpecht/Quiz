7cf556a25c60004d6648fbc28746326b1b1a80fe
Cornelius Specht <corneliusspecht@me.com>
2015-12-22 19:04:57 +0100

    Remove CSP headers

--- middleware/security-headers.js
+++ middleware/security-headers.js
@@ -1,22 +1,5 @@
-var cspb = require( 'content-security-policy-builder' )
-var cspHeader = cspb({
-  directives: {
-    reportUri: '/-/',
-    defaultSrc: ["'self'"],
-    scriptSrc: ["'self'"],
-    styleSrc: ["'self'", "'unsafe-inline'"],
-    connectSrc: ["'self'", 'wss:'],
-    childSrc: ["'none'"],
-    frameSrc: ["'none'"],
-    imgSrc: ["'self'", 'data:'],
-    sandbox: ['allow-forms', 'allow-scripts'],
-  }
-})
-
 module.exports = function( req, res, next ) {
 
-  res.setHeader( 'Content-Security-Policy-Report-Only', cspHeader )
-  // res.setHeader( 'Content-Security-Policy', cspHeader )
   res.setHeader( 'X-XSS-Protection', '1; mode=block' )
   res.setHeader( 'X-Frame-Options', 'DENY' )
   res.setHeader( 'X-Download-Options', 'noopen' )
