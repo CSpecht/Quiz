7086b1ba3ab0e399de9d746691892df3c6338304
Cornelius Specht <corneliusspecht@me.com>
2015-12-22 23:22:18 +0100

    Update auth/login: Impl LDAP auth req

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -1,7 +1,30 @@
-function validate( user, password, callback ) {
-  callback( null, false )
-}
+var request = require( 'request' )
+var debug = require( 'debug' )( 'clicker:auth:login' )
 
 module.exports = function login( req, res, next ) {
-  next( new Error( 'Not implemented' ) )
+
+  debug( 'request', req.body )
+
+  request.post({
+    url: 'https://mars.iuk.hdm-stuttgart.de/~ck115/login.php',
+    resolveWithFullResponse: true,
+    form: {
+      user: req.body.user || '',
+      password: req.body.password || '',
+    },
+  }, function( error, httpResponse, body ) {
+
+    if( error ) {
+      res.status( 403 )
+      res.send({ message: error.message })
+      return
+    }
+
+    debug( httpResponse.statusCode, body )
+
+    res.status( httpResponse.statusCode )
+    res.end()
+
+  })
+
 }
