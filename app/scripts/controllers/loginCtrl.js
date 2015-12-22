15f00fef1ec69f39203c934897a5638fc6956db0
Cornelius Specht <corneliusspecht@me.com>
2015-12-23 00:20:37 +0100

    Update loginCtrl: Fix form data

--- app/scripts/controllers/loginCtrl.js
+++ app/scripts/controllers/loginCtrl.js
@@ -11,20 +11,13 @@
 
       $scope.submit = function () {
 
-        var data = {
-          username: $scope.username,
-          password: $scope.password,
-        }
-
-        console.log( 'AUTH DATA', data, $scope.login, this )
-
         $rootScope.loggedInUser = $scope.username;
         //console.log($scope.login);
         //console.log($scope.password);
 
         $http({
           method: 'POST',
-          data: data,
+          data: $scope.login,
           url: nodeServer + '/auth'
         }).then(function successCallback(response) {
           console.log( response )
