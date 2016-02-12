c57c4ae10e9ae02ce996647ede118c98e71bdff0
Cornelius Specht <corneliusspecht@me.com>
2016-02-12 22:52:37 +0100

    Update middleware/session: Add deprecated default options

--- middleware/session.js
+++ middleware/session.js
@@ -6,6 +6,9 @@ module.exports = session({
   name: 'session',
   unset: 'destroy',
   secret: 'jesus loves no one',
+  resave: false,
+  rolling: true,
+  saveUninitialized: false,
   store: new SessionStore({
     ttl: 14 * 24 * 60 * 60,
     mongooseConnection: mongoose.connection,
